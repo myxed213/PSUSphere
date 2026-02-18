e600f3e32da0c7ad9878c8db639864d67bab556e
Janry Cedie <you@example.com>
2026-02-19 00:00:41 +0800

    commit6

--- studentorg/management/commands/create_initial_data.py
+++ studentorg/management/commands/create_initial_data.py
@@ -6,9 +6,32 @@ class Command(BaseCommand):
     help = 'Create initial data for the application'
 
     def handle(self, *args, **kwargs):
+        self.ensure_basic_data()
         self.create_organization(10)
         self.create_students(50)
         self.create_membership(10)
+
+    def ensure_basic_data(self):
+        colleges = [
+            'College of Arts and Sciences', 'College of Engineering',
+            'College of Computer Studies', 'College of Business and Accountancy',
+            'College of Education', 'College of Agriculture',
+            'College of Nursing', 'College of Hospitality Management'
+        ]
+        for c_name in colleges:
+            College.objects.get_or_create(college_name=c_name)
+
+        programs = [
+            ('BS in Computer Science', 'College of Computer Studies'),
+            ('BS in Information Technology', 'College of Computer Studies'),
+            ('BS in Civil Engineering', 'College of Engineering'),
+            ('BS in Mechanical Engineering', 'College of Engineering'),
+            ('BA in English', 'College of Arts and Sciences'),
+            ('BS in Biology', 'College of Arts and Sciences')
+        ]
+        for p_name, c_name in programs:
+            college = College.objects.get(college_name=c_name)
+            Program.objects.get_or_create(prog_name=p_name, college=college)
 
     def create_organization(self, count):
         fake = Faker()
