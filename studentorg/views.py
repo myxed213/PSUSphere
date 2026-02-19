accd5fc99083154fd7ee2ce816ed067075cd2638
Janry Cedie <you@example.com>
2026-02-19 09:31:16 +0800

    Commit7

--- studentorg/views.py
+++ studentorg/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Welcome to PSUSphere</h1><p>The student organization management system is running.</p>")
+    return render(request, 'studentorg/index.html')
