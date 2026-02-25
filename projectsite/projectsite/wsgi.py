965b85a6e421dfff69fa16f6ed42e9ccdc7212b5
Janry Cedie <you@example.com>
2026-02-25 21:34:18 +0800

    Fix path issues for PythonAnywhere deployment

--- projectsite/projectsite/wsgi.py
+++ projectsite/projectsite/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for projectsite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+from pathlib import Path
 
-from django.core.wsgi import get_wsgi_application
+# Add the project directory to the sys.path
+path = Path(__file__).resolve().parent.parent
+if str(path) not in sys.path:
+    sys.path.append(str(path))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectsite.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
